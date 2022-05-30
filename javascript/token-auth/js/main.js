e63e9b8f361a9b10dcc699bd1594f582c0a60c18
Or Yaakov <oryaakov16@gmail.com>
2022-05-30 21:08:45 +0300

    token auth solution

---
+++ javascript/token-auth/js/main.js
@@ -0,0 +1,121 @@
+const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+const $ = {
+    main: document.querySelector("main"),
+    email: document.getElementById("email"),
+    pass: document.getElementById("pass"),
+}
+
+const users = localStorage.getItem("users");
+let usersJSON = users ? JSON.parse(users) : [];
+
+let user = localStorage.getItem("user");
+let userJSON = user ? JSON.parse(user) : {};
+
+tokenLogin();
+
+function login() {
+
+    if ($.email.value && $.pass.value) {
+        const uniqueToken = generateToken();
+
+        const newUser = {
+            email: $.email.value,
+            pass: $.pass.value,
+            token: uniqueToken
+        }
+
+        for (let i = 0; i < usersJSON.length; i++) {
+            const u = usersJSON[i];
+
+            if (u.email === newUser.email && u.pass === newUser.pass) {
+                newUser.fName = usersJSON[i].fName;
+                usersJSON[i] = newUser;
+
+                localStorage.setItem("user", JSON.stringify(newUser))
+                localStorage.setItem("users", JSON.stringify(usersJSON));
+
+                $.main.innerHTML = `Welcome ${u.fName}`;
+
+                break
+            }
+        }
+
+    } else {
+        alert("Fill the all fields please.");
+    }
+}
+
+function tokenLogin() {
+    const uniqueToken = generateToken();
+
+    for (let i = 0; i < usersJSON.length; i++) {
+        const u = usersJSON[i];
+
+        if (userJSON.email === u.email && userJSON.token === u.token) {
+            u.token = uniqueToken;
+            usersJSON[i] = u;
+
+            localStorage.setItem("user", JSON.stringify(u))
+            localStorage.setItem("users", JSON.stringify(usersJSON));
+
+            $.main.innerHTML = `Welcome Back ${u.fName}`;
+
+            break
+        }
+    }
+}
+
+function generateToken() {
+    let token = '';
+
+    for (let i = 0; i < 15; i++) {
+        const number = new Date().getTime() % 100;
+        const random = parseInt(Math.random() * chars.length);
+
+        let randomIndex = number > random ? number - random : random - number;
+
+        if (randomIndex > chars.length - 1) {
+            randomIndex -= 37;
+        }
+
+        const char = chars[randomIndex]
+        token += char;
+    }
+
+    return token;
+}
+
+// const users1 = [
+//     {
+//         fName: "Or",
+//         email: "or@gmail.com",
+//         pass: "or123",
+//         token: ""
+//     },
+//     {
+//         fName: "David",
+//         email: "david@gmail.com",
+//         pass: "david123",
+//         token: ""
+//     },
+//     {
+//         fName: "Jimmy",
+//         email: "jimmy@gmail.com",
+//         pass: "jimmy123",
+//         token: ""
+//     },
+//     {
+//         fName: "Esti",
+//         email: "esti@gmail.com",
+//         pass: "esti123",
+//         token: ""
+//     }
+// ]
+
+// for (let i = 0; i < users1.length; i++) {
+//     const user = users1[i];
+//     user.token = generateToken();
+// }
+
+// localStorage.setItem("users", JSON.stringify(users1))
