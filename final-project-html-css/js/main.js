4c8b00d262e9264c0a0cff215c424d284c21520f
Or Yaakov <oryaakov16@gmail.com>
2022-06-03 19:48:49 +0300

    .

--- final-project-html-css/js/main.js
+++ final-project-html-css/js/main.js
@@ -1,2 +1,2 @@
 const copyRightDate = document.getElementById("date");
-copyRightDate.innerText += new Date().getFullYear().toString();
+copyRightDate.innerText += " " + new Date().getFullYear().toString();
